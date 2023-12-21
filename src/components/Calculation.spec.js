386d19d5710121c2b6e0b99d26e9cf33c3e86b63
Charlotte Gracia <56256920+charlottegracia@users.noreply.github.com>
2023-12-21 15:45:58 +0100

    we testin again

--- src/components/Calculation.spec.js
+++ src/components/Calculation.spec.js
@@ -1,11 +1,53 @@
-import {mount} from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Calculation from "../components/Calculation.vue";
 import { expect, test } from "vitest";
 
 const wrapper = mount(Calculation);
+let mockData = {
+        receivedValue: {
+            contractValues: {
+                salePrice: 25000,
+                cost: '',
+                estimatedMarketValue: '',
+                residualValue: 15000,
+                cashPrice: 30000,
+                runningTime: '',
+                activeRunningTime: '',
+                interestRate: '',
+                contractCreation: '',
+                oneTimeBenefit: '',
+                deposit: '',
+                depreciation: '',
+                commission: '',
+                privateShare: '',
+                registrationFee: ''
+            },
+            customer: {
+                name: '',
+                email: '',
+                under25: '',
+                customerType: '',
+                contractType: '',
+                startDate: '2023-12-24',
+                season: '',
+                import: '',
+            },
+            vehicle: {
+                frameNumber: '',
+                vehicle: '',
+                newVehicle: '',
+                firstRegistrationDate: '',
+                initialPrice: '',
+                vatDeath: '',
+                vehicleType: '',
+                levyPaid: true,
+                mileage: '',
+            }
+        }
+}
 
-describe('Calculation', () => {
-    test('Calculation should have an h2 with text "Beregning"', () => {
+describe('carPrice', () => {
+    test('should have an h2 with text "Beregning"', () => {
         // Assert that the h2 element exists
         expect(wrapper.find('h2').exists()).toBe(true);
     
@@ -13,85 +55,101 @@ describe('Calculation', () => {
         expect(wrapper.find('h2').text()).toBe('Beregning');
     });
 
-    test('computed property carPrice is calculated correctly for Genleasing contract type', () => {
-        // Create a new Vue instance with the desired data
-        const mockData = {
-            receivedValue: {
-                customer: {
-                    contractType: 'Genleasing',
-                },
-                contractValues: {
-                    cashPrice: 30000,
-                    residualValue: 15000,
-                    salePrice: 25000,
-                },
-                vehicle: {
-                    levyPaid: true
-                }
-            },
-        };
-      
-        // Mount the component with the mocked Vue instance
+    test('is calculated correctly for Genleasing contract type', () => {
+        mockData.receivedValue.customer.contractType = 'Genleasing';
+
         const wrapper = mount(Calculation, { data: () => mockData });
-      
-        // Access the computed property
         const computedValue = wrapper.vm.carPrice;
       
-        // Assert the result for Genleasing contract type
-        expect(computedValue).toBe(15000); // Adjust the expected value based on your computation
+        expect(computedValue).toBe(15000);
     });
-    test('computed property carPrice is calculated correctly for Pristjek contract type', () => {
-        // Create a new Vue instance with the desired data
-        const mockData = {
-            receivedValue: {
-                customer: {
-                    contractType: 'Pristjek',
-                },
-                contractValues: {
-                    cashPrice: 30000,
-                    residualValue: 15000,
-                    salePrice: 25000,
-                },
-                vehicle: {
-                    levyPaid: true
-                }
-            },
-        };
+
+    test('is calculated correctly for Pristjek contract type', () => {
+        mockData.receivedValue.customer.contractType = 'Pristjek';
       
-        // Mount the component with the mocked Vue instance
         const wrapper = mount(Calculation, { data: () => mockData });
-      
-        // Access the computed property
         const computedValue = wrapper.vm.carPrice;
       
-        // Assert the result for Genleasing contract type
-        expect(computedValue).toBe(30000); // Adjust the expected value based on your computation
+        expect(computedValue).toBe(30000);
     });
-    test('computed property carPrice is calculated correctly for Nytegning contract type', () => {
-        // Create a new Vue instance with the desired data
-        const mockData = {
-            receivedValue: {
-                customer: {
-                    contractType: 'Nytegning',
-                },
-                contractValues: {
-                    cashPrice: 30000,
-                    residualValue: 15000,
-                    salePrice: 25000,
-                },
-                vehicle: {
-                    levyPaid: true
-                }
-            },
-        };
+
+    test('is calculated correctly for Nytegning contract type', () => {
+        mockData.receivedValue.customer.contractType = 'Nytegning';
       
-        // Mount the component with the mocked Vue instance
         const wrapper = mount(Calculation, { data: () => mockData });
-      
-        // Access the computed property
         const computedValue = wrapper.vm.carPrice;
       
-        // Assert the result for Genleasing contract type
-        expect(computedValue).toBe(25000); // Adjust the expected value based on your computation
+        expect(computedValue).toBe(25000);
+    });
+});
+
+describe('startDateComp', () => {
+    test('should return an Date object', () => {
+        mockData.receivedValue.customer.startDate = '2023-12-24';
+
+        const wrapper = mount(Calculation, { data: () => mockData });
+        const computedValue = wrapper.vm.startDateComp;
+      
+        expect(computedValue).toBeInstanceOf(Date);
+    });
+
+    test('should return the correct Date object', () => {
+        const expectedDate = new Date('2023-12-24T00:00:00.000Z');
+    
+        mockData.receivedValue.customer.startDate = '2023-12-24';
+    
+        const wrapper = mount(Calculation, { data: () => mockData });
+    
+        const computedValue = wrapper.vm.startDateComp;
+    
+        expect(computedValue).toEqual(expectedDate);
+      });
+});
+
+
+describe('carMonths', () => {
+    test('should calculate the correct monthsOver36 and monthsUnder36', () => {
+        mockData.receivedValue.vehicle.firstRegistrationDate = '2021-12-01';
+        mockData.contractRunTime = 36;
+        mockData.startDateComp = '2024-04-01';
+        
+        const wrapper = mount(Calculation, { data: () => mockData });
+        
+
+        const computedValue = wrapper.vm.carMonths;
+    
+        expect(computedValue.monthsOver36).toBe(28);
+        expect(computedValue.monthsUnder36).toBe(8);
+    });
+});
+
+
+describe('carAge', () => {
+    test('should calculate correct car age in months', () => {
+        mockData.receivedValue.vehicle.firstRegistrationDate = '2021-06-01';
+        mockData.registrationDate  = new Date(mockData.receivedValue.vehicle.firstRegistrationDate);
+        mockData.today = new Date('2023-12-01');
+
+        const wrapper = mount(Calculation, { data: () => mockData });
+
+        const computedValue = wrapper.vm.carAge;
+
+        expect(computedValue).toBe(30);
+    });
+});
+
+describe('momsMonth', () => {
+    test('should calculate correct Moms fradrag i måneden', () => {
+        mockData.receivedValue.contractValues.registrationFee = 100000;
+        mockData.carMonths = {
+            monthsUnder36: 12,
+            monthsOver36: 0
+        }
+
+        const wrapper = mount(Calculation, { data: () => mockData });
+
+        const computedValue = wrapper.vm.momsMonth;
+
+        expect(computedValue).toBe((6000).toFixed(2));
     });
 });
