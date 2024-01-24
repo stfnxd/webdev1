4cdc4978899c42105245b3daff08a290e54c9f2a
Charlotte Gracia <56256920+charlottegracia@users.noreply.github.com>
2024-01-24 21:44:06 +0100

    fix så test virker

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,11 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'happy-dom',
-    }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
 })
+
